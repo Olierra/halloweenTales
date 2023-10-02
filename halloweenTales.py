8dde405b06e32afb2f1e200027e332c3eea6024e
Chance Bailey <olierrathegravespecter@gmail.com>
2023-10-02 16:41:11 -0500

    Version 0.2 Choosing a Story and the Random Story are working. Still need to build starter stories and a list of saved stories (in files) to finish option 3.

--- halloweenTales.py
+++ halloweenTales.py
@@ -18,65 +18,101 @@
     #Wrap anything that can break in try/except to catch errors.
 """
 
+import random
+from re import T
 from sys import exit
 from os import system
-
-testDictionary = {
-    'key1' : 'value1',
-    'key2' : 'value2'
-}
+from random import choice
 
 def main():
+    storyList = {
+        '1': {
+            'title':'The Case of the Missing Candy',
+            'body':'lorem ipsum PH1 PH2 PH3 PH4 PH5 PH6 PH7 PH8 PH9',
+            'inputs': {'PH1':'text','PH2':'text','PH2':'text','PH3':'text','PH4':'text','PH5':'text','PH6':'text','PH7':'text','PH8':'text','PH9':'text'}
+            },
+        '2': {
+            'title':'The Best Costume of the Night',
+            'body':'lorem ipsum',
+            'inputs': {'PH1':'text','PH2':'text','PH2':'text','PH3':'text','PH4':'text','PH5':'text','PH6':'text','PH7':'text','PH8':'text','PH9':'text'}
+        },
+        '3': {
+            'title':'Family Pumpkin Carving',
+            'body':'lorem ipsum',
+            'inputs': {'PH1':'text','PH2':'text','PH2':'text','PH3':'text','PH4':'text','PH5':'text','PH6':'text','PH7':'text','PH8':'text','PH9':'text'}
+        },
+        '4': {
+            'title':'Scary Story Night',
+            'body' : 'lorem ipsum',
+            'inputs': {'PH1':'text','PH2':'text','PH2':'text','PH3':'text','PH4':'text','PH5':'text','PH6':'text','PH7':'text','PH8':'text','PH9':'text'}
+        },
+        '5': {
+            'title':'Baking Ghosts?!?',
+            'body':'lorem ipsum',
+            'inputs': {'PH1':'text','PH2':'text','PH2':'text','PH3':'text','PH4':'text','PH5':'text','PH6':'text','PH7':'text','PH8':'text','PH9':'text'}
+        }
+    }
+    menu = 'Welcome to Halloween Tales\n\n1. Choose a Story.\n2. Start a Random Story.\n3. Read a Finished Story.\n4. Exit\n'
     menuRange = range(1,5)
     userChoice = 0
-
-    try:
-        while int(userChoice) not in menuRange: #while the user hasn't chose a number between 1 and 4, keep reprinting the menu
-            system('cls') #clears the screen. Make sure you start without debugging or it doesn't work.
-            print('welcome to Halloween Tales!\n')
-            print('1. Choose a Story.')
-            print('2. Start a Random Story.')
-            print('3. Read a Finished Story.')
-            print('4. Exit\n')
-
-            userChoice = int(input('Please choose an option from the menu: ')) #Any value here not a number throws the error we're catching below.
-    except:
+    while int(userChoice) not in menuRange: #while the user hasn't chose a number between 1 and 4, keep reprinting the menu
         system('cls') #clears the screen. Make sure you start without debugging or it doesn't work.
-        print('welcome to Halloween Tales!\n')
-        print('1. Choose a Story.')
-        print('2. Start a Random Story.')
-        print('3. Read a Finished Story.')
-        print('4. Exit\n')
-
-        userChoice = input('Please enter the number of your option: ')
-
+        print(menu)
+        try:
+            userChoice = int(input('Please choose an option from the menu: ')) #Any value here not a number throws the error we're catching below.
+        except:
+            continue #Sends the user back to the menu until they enter a number on the menu.
     if userChoice == 1:
         system('cls')
-        print('You chose option 1!')
+        storyMenu(storyList)
     elif userChoice == 2:
         system('cls')
-        print('You chose option 2!')
+        getUserText(storyList[choice(list(storyList))])
     elif userChoice == 3:
         system('cls')
-        print('You chose option 3!')
-    else:
+        storyList()
+    elif userChoice == 4:
+        print('Goodbye!')
         exit(0)
     return 0
 
-def replaceText(story, textDictionary):
-    originalText = story #'key1 key2'
-    replacementWords = textDictionary #{'key1':'value1','key2':'value2'}
+def storyMenu(storyList):
+    stories = storyList
+    storyRange = range(1,(len(stories)+1))
+    storyChoice = 0
+    print('Available Stories\n')
+    while int(storyChoice) not in storyRange:
+        system('cls')
+        for story in stories:
+            print(story + '. ' + str(stories[story]['title']))
+        try:
+            storyChoice = int(input('\nPlease choose a story: '))
+        except:
+            continue
+    return getUserText(stories[str(storyChoice)])
+    
+def storyList():
+    print('You made it to the story list!')
+
+def getUserText(story):
+    userStoryTitle = story['title']
+    userStoryBody = story['body']
+    userDictionary = story['inputs']
+    replacementList = {}
+    print('Your story is: ' + userStoryTitle + '\n')
+
+    for key in userDictionary:
+        tempInput = input(f'Please enter ' + key + ": ")
+        replacementList[key] = tempInput
+        
+    return(replaceText(userStoryBody, replacementList))
+
+def replaceText(story, userInputs):
+    originalText = story
+    replacementWords = userInputs
     for key in replacementWords:
         originalText = originalText.replace(key, replacementWords[key])
     return print(originalText)
-
-def getUserText():
-    userStory = ''
-    userDictionary = {
-        'a': 'b',
-        'c': 'd'
-    }
-    return(replaceText(userStory, userDictionary))
 
 main()
 
